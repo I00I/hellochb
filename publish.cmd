3ba6497b0e70b112200ceecae6c5984ff8df1017
Chidirala, Ramesh <Ramesh.Chidirala@wolterskluwer.com>
2018-05-01 21:07:04 -0500

    iniital commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=hellochatbot2-b1f2-Web-Deploy.pubxml -p:Password=mJjLsdbulPyzowqY2mKqWYgKYwLou9oXXrm66hCid1hvj0xmej6nAYwler7Q
+
